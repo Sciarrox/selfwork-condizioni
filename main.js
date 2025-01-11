33645182987a5f1dc429144e606586d01373581e
Sciarrox <sciarrox@gmail.com>
2025-01-11 07:16:51 +0100

    selfwork condizioni

--- main.js
+++ main.js
@@ -14,5 +14,29 @@ if (voto < 18) {
     console.log('eccellente');
 }else{
     console.log('Voto non valido');
+}
 
+let v = 29;
+
+switch (true) {
+    case (v < 18):
+        console.log("insufficiente");
+        break;
+    case (v >= 18 && v < 21):
+        console.log("sufficiente");
+        break;
+    case (v >= 21 && v < 24):
+        console.log("buono");
+        break;
+    case (v >= 24 && v < 27):
+        console.log("distinto");
+        break;
+    case (v >= 27 && v <= 29):
+        console.log("ottimo");
+        break;
+    case (v === 30):
+        console.log("eccellente");
+        break;
+    default:
+        console.log("Valore non valido");
 }
